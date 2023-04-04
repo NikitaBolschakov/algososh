ed5d953fe1ccdd5e77a7203a66a8e6aa1524120f
Nikita Bolshakov <itbolshakov@yandex.ru>
2023-04-04 13:56:18 +0300

    fix: deploy. Add props basename in BrouserRouter

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -13,7 +13,7 @@ import "./app.css";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" exact>
             <MainPage />
